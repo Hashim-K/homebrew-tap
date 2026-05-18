c8439d0df8ca63fb3f94ebbdf9765610cac51619
Hashim Karim <hashimkarim168@gmail.com>
2026-05-18 03:37:09 +0200

    Update usagestat to 1.0.2

--- Formula/usagestat.rb
+++ Formula/usagestat.rb
@@ -1,22 +1,23 @@
 class Usagestat < Formula
   desc "Scriptable CLI for local agent usage data"
   homepage "https://github.com/Hashim-K/usagestat"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
   depends_on :linux
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Hashim-K/usagestat/releases/download/v#{version}/usagestat-linux-aarch64.tar.gz"
-      sha256 "6c850a17c0aaf2c55b8faa44a4827f2fa63d98235970314e42fbfe2f915f2e06"
+      sha256 "bd6e75663973f4535494604987b88cb3928b7ead2de8a7bf269b144e80b866c9"
     else
       url "https://github.com/Hashim-K/usagestat/releases/download/v#{version}/usagestat-linux-x86_64.tar.gz"
-      sha256 "c6999f50dabd03a595edae6d908a5f588b0e1feb6a69588656612cb0532e96e5"
+      sha256 "50aa23ee61a3a19e38ef63d253d1ac7228c511f615ca0f9197553bc3cfc0a530"
     end
   end
 
   def install
     bin.install "usagestat"
+    (share/"usagestat").install "plugins"
   end
 
   test do
